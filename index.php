3793812b4312db684925021ff82fe1dfe8928125
Ja0zinh0 <163667621+Ja0zinh0@users.noreply.github.com>
2024-04-25 10:23:52 -0300

    update
    anotações da aula

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- e necessario ter um servidor -->
+<!-- servidor mais utilizado APACHE -->
+<!-- o nome do arquivo sempre tem que ser index -->
+<!-- o php pode ser trabalhado junto com o html -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <table border="1"width="200px">
+        <tr>
+            <td>
+                <?php
+                    echo"Primeira aula  <br> ";
+                    echo "curso de PHP";
+                ?>
+
+            </td>
+        </tr>
+    </table>
+    
+</body>
+</html>
