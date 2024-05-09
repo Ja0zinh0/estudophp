db96fd1f9e7f8f8f3df6d8647f9b3e1acc27b130
AlunoETC <alunoetc@EDUC.NET>
2024-05-09 11:40:18 -0300

    update

---
+++ exercicio3.php
@@ -0,0 +1,20 @@
+<?php
+$numeroRodas = 10;
+switch ($numeroRodas) {
+    case 2:
+        echo "uma moto";
+        break;
+    case 3:
+        echo "um triciculo";
+        break;
+    case 4:
+        echo "O veículo é um carro.";
+        break;
+    case 18:
+        echo "O veículo é um caminhão.";
+        break;
+    default:
+        echo " não possui nenhum veiculo ";
+        break;
+}
+?>
