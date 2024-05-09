de143c8cc2e9cff2fe7155438d9ae1abe58a46e6
AlunoETC <alunoetc@EDUC.NET>
2024-05-09 11:30:58 -0300

    update
    aqui mostra como fazer um switchcase em php que e como se fosse a função de python

---
+++ switchcase.php
@@ -0,0 +1,30 @@
+<?php
+$diaDaSemana = 7;
+switch ($diaDaSemana) {
+    case 1:
+        echo "Domingo";
+        break;
+    case 2:
+        echo "Segunda";
+        break;
+    case 3:
+        echo "terça";
+        break;
+    case 4:
+        echo "quarta";
+        break;
+    case 5:
+        echo "quinta";
+        break;
+    case 6:
+        echo "sexta";
+        break;
+    case 7:
+        echo "sábado";
+        break;
+    default:
+        echo "Dia inválido";
+        break;
+}
+
+?>
