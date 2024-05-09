de63625281ff6bed861a0df2bfcc80f512e37b79
João Silva <163667621+Ja0zinh0@users.noreply.github.com>
2024-05-09 11:10:07 -0300

    Atualizar o exerfic2.php

--- exerfic2.php
+++ exerfic2.php
@@ -4,9 +4,10 @@ $altura = 1.85;
 
 if ($sexo == "masculino") {
     $pesoideal = (72.7 * $altura) - 58;
-    echo "seu peso ideal é ". $pesoideal;
+    
 }
-elseif ($sexo == "feminino") {
+ else {
     $pesoideal = (62.1* $altura) - 44.7;
 }
+echo "seu peso ideal é ".$pesoideal;
 ?>
